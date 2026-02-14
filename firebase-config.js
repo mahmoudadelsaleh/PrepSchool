b1c90803d082ba8b2b3a418fc8d6d215d1ea9168
Mahmoud Adel Saleh <mahmoudadelsaleh@gmail.com>
2026-02-14 13:14:49 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDUiv_BJJWjpRjU7UHEWIFig1hSMuD6-uU",
+  authDomain: "prepschool-c5ff7.firebaseapp.com",
+  projectId: "prepschool-c5ff7",
+  storageBucket: "prepschool-c5ff7.firebasestorage.app",
+  messagingSenderId: "505401111134",
+  appId: "1:505401111134:web:fcdca4c6d930ba82603acf",
+  measurementId: "G-WQ3PWF49HG"
 };
-// Initialize Firebase
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-}
-const db = firebase.firestore();
