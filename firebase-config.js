8ac3372fb3331a0f21b75945c352120fa485f3a6
Mahmoud Adel Saleh <mahmoudadelsaleh@gmail.com>
2026-02-14 13:33:47 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDUiv_BJJWjpRjU7UHEWIFig1hSMuD6-uU",
   authDomain: "prepschool-c5ff7.firebaseapp.com",
@@ -8,3 +7,9 @@ const firebaseConfig = {
   appId: "1:505401111134:web:fcdca4c6d930ba82603acf",
   measurementId: "G-WQ3PWF49HG"
 };
+
+// تفعيل فايربيز (تأكد من وجود هذا الجزء)
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+const db = firebase.firestore();
